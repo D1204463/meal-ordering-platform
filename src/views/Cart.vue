066b0d7fef8457503cd5bbe7ad31ddaeebd6d1f4
D1204463 <161418099+D1204463@users.noreply.github.com>
2024-08-28 16:45:43 +0800

    新增餐廳圖片

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -68,9 +68,9 @@ export default {
   methods: {
     generateMockCartItems() {
       return [
-        { id: 1, name: 'Item 1', price: 10.00, quantity: 2, description: 'This is a great item.', image: 'path/to/image1.jpg' },
-        { id: 2, name: 'Item 2', price: 20.00, quantity: 1, description: 'This item is even better!', image: 'path/to/image2.jpg' },
-        { id: 3, name: 'Item 3', price: 5.50, quantity: 4, description: 'An affordable choice.', image: 'path/to/image3.jpg' },
+        { id: 1, name: 'Item 1', price: 10.00, quantity: 2, description: 'This is a great item.', image: '/img/Japan.jpg' },
+        { id: 2, name: 'Item 2', price: 20.00, quantity: 1, description: 'This item is even better!', image: '/img/Italy.jpg' },
+        { id: 3, name: 'Item 3', price: 5.50, quantity: 4, description: 'An affordable choice.', image: '/img/Chinese.jpg' },
       ];
     },
     removeFromCart(itemId) {
